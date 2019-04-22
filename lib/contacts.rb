ae76f5ad53cc2aa080afdefed3fd67a5daef7c2a
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-22 00:34:56 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -12,15 +12,15 @@ require 'pry'
   #     email: DATA**** "freddy@mercury.com",
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
-  # } FIRST ITERATE OVER THE CONTACTS HASH AND FIND THE NAME AND DETAIL HEADING-RETURNING HASH INFO. NEXT ITERATE OVER THE DETAILS HEADING AND SCAN FOR THE :ATTRIBUTE WITH LINKED DATA. THEN SCAN THE DATA AND APPLY A CONDITIONAL IF IT FINDS A == ATTRIBUTE: 
+  # } FIRST ITERATE OVER THE CONTACTS HASH AND FIND THE NAME AND DETAIL HEADING-RETURNING HASH INFO. NEXT ITERATE OVER THE DETAILS HEADING AND SCAN FOR THE :ATTRIBUTE WITH LINKED DATA. THEN SCAN THE DATA AND APPLY A CONDITIONAL IF IT FINDS A == ATTRIBUTE: of ice cream THEN DELETE THAT DATA |ATTRIBUTE, DATA| 
   
 def remove_strawberry(contacts)
   contacts.each do |person, details|
     details.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        binding.pry
-  
-end
-end
-end
+       data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end 
+      end
+    end
+  end
 end
