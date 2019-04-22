689659b75d71eafc65e21d7a160f380458258775
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-22 00:38:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -12,7 +12,7 @@ require 'pry'
   #     email: DATA**** "freddy@mercury.com",
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
-  # } FIRST ITERATE OVER THE CONTACTS HASH AND FIND THE NAME AND DETAIL HEADING-RETURNING HASH INFO. NEXT ITERATE OVER THE DETAILS HEADING AND SCAN FOR THE :ATTRIBUTE WITH LINKED DATA. THEN SCAN THE DATA AND APPLY A CONDITIONAL IF IT FINDS A == ATTRIBUTE: of ice cream THEN DELETE THAT DATA |ATTRIBUTE, DATA| 
+  # } FIRST ITERATE OVER THE CONTACTS HASH AND FIND THE NAME AND DETAIL HEADING-RETURNING HASH INFO. NEXT ITERATE OVER THE DETAILS HEADING AND SCAN FOR THE :ATTRIBUTE WITH LINKED DATA. THEN SCAN THE DATA AND APPLY A CONDITIONAL IF IT FINDS A == ATTRIBUTE: of ice cream THEN DELETE THAT DATA |ATTRIBUTE, DATA|. |ice_cream| placeholder variable for value retrieved from :favorite ice cream flavors key. 
   
 def remove_strawberry(contacts)
   contacts.each do |person, details|
@@ -24,4 +24,4 @@ def remove_strawberry(contacts)
     end
   end
   
-  
+  #running ruby bin/contacts in terminal runs the test with this method. make sure that strawberry has been ommitted.
